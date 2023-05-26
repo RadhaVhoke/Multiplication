0f5ef0b2f24e0ea48a9ba8083eeb004ac2ca52c5
radhavhoke921 <radhavhoke921@gmail.com>
2023-05-26 11:15:43 +0530

    conflict example

--- src/com/demo/Multi.java
+++ src/com/demo/Multi.java
@@ -11,14 +11,24 @@ public class Multi {
       b=40;
       System.out.println("Multiplication :"+(a*b));
 		
-	// added from github
+	// added from github by sujit
       a=50;
       b=60;
       System.out.println("Multiplication :"+(a*b));
       
+      // added from local by ajit
       a=70;
       b=80;
       System.out.println("Multiplication :"+(a*b));
+      
+      // added from local by ajit for conflict example
+      // bcz sujit have added some code 
+      //but we have not pulled those changes in our eclipse
+      // and we are trying push our some changes to github
+      a=90;
+      b=100;
+      System.out.println("Multiplication :"+(a*b));
+		
 		
 		
 	}
